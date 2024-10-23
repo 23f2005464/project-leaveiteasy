09045a618a2c3af798b465435e11051f2b930341
KT252005 <23f2005464@ds.study.iitm.ac.in>
2024-10-23 21:37:30 +0530

    latest 23/09

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 
 urlpatterns = [
-   path("",views.home,name="mainapp_home")
+   path("",views.home,name="mainapp_home"),
+   path("home/404/",views.notfound_page,name="page_not")
 ]
--- home/views.py
+++ home/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,"home.html")
+    return render(request,"landing_page.html")
+
+def notfound_page(request):
+    return render(request,"404.html")
+    
